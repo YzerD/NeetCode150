b0ca5e3c51dfcb0b8804041f5679d3d7e3770f2f
YzerD <yzerdegula@gmail.com>
2023-08-14 13:17:49 -0400

    Updates UMPIRE Portion

--- BinarySearch/KokoEatingBananas.py
+++ BinarySearch/KokoEatingBananas.py
@@ -7,13 +7,65 @@ import math
 
 """
 U-nderstand:
-
-
+    # ? What is our input ?
+        Our input is a list of piles of bananas and an integer h which is the
+    number of hours Koko has to eat all the bananas.
+    # ? What is our output ?
+        Our output is an integer which is the minimum integer Koko can eat per
+    hour to finish all the bananas within h hours.
+    # ? Do we have constraints ?
+        1 <= piles.length <= 10^4
+        piles.length <= h <= 10^9
+        1 <= piles[i] <= 10^9
+    # ? Can our input be empty ?
+        No, our input cannot be empty. The constraints state that piles.length
+    is at least 1.
+    
 M-atch:
+        For this problem we can initially started off by using a brute force 
+    approach. We can iterate through the piles and for each pile we can
+    calculate the time it takes to eat the pile. We can then add the time to
+    a total time variable. If the total time is less than or equal to h, we
+    can return the current value of k. If the total time is greater than h,
+    we can return the max pile. This solution will work but it will be slow.
+
+        We can optimize this solution by using binary search. We can declare two
+    pointers, left and right. Left will be 1 and right will be the max pile.
+    We can then calculate the mid (eating speed) by adding left and right and
+    dividing by 2. We can then iterate through the piles and calculate the
+    total time it takes to eat all the piles. If the total time is less than
+    or equal to h, we can update our result to be the minimum of our current
+    result and k. We can then update our right pointer to be k - 1. If the
+    total time is greater than h, we can update our left pointer to be k + 1.
+    We can continue this process until our left pointer is greater than our
+    right pointer. We can then return our result.
 
 
 P-lan:
+    Brute Force:
+    1) Get max pile
+    2) Iterate from 1 to max pile
+    3) Declare total time
+    4) Iterate through piles
+    5) Add time to total time
+    6) If total time is less than or equal to h,
+    7) return k
+    8) If total time is greater than h, return max pile
 
+    Binary Search:
+    1) Delcare our two pointers
+    2) Declare our result variable
+    3) While our two pointers havent crossed
+    4) Calculate our mid (eating speed)
+    5) Declare our total time
+    6) Iterate through piles
+    7) Add time to total time
+    8) If total time is less than or equal to h,
+    9) Take the min of our result and k
+    10) Update our right pointer
+    11) Otherwise, we know that k is too small
+    12) Update our left pointer
+    13) Return our result
 
 I-mplement:
 """
