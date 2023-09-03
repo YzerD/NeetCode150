28366334df62858fa999577e6ff5aa7df8b26e56
YzerD <yzerdegula@gmail.com>
2023-09-03 13:07:02 -0400

    Finishes Subsets

--- Backtracking/Subsets.py
+++ Backtracking/Subsets.py
@@ -22,6 +22,9 @@ U-nderstand:
     # ? What values can the elements in our input array have ?
         The constraints state that the possible values that our elements in our
     input array can take is -10 to 10.
+    # ? Are duplicate subsets allowed ?
+        No, The problem specifies that the solution set must not contain
+    duplicate subsets.
 
 M-atch:
     For strings and arrays, common patterns for solutions include:
@@ -30,10 +33,49 @@ M-atch:
         3) Sorting
         4) Sliding Window
     
-        For this problem.
-
-
+        For this problem, none of the common patterns are applicable for this
+    problem. This is because for subsets there are 2^n different possible
+    subsets. Since we want the actual subsets rather than the number of subsets
+    the most efficient our algorithm can be is O(n * 2^n). We can use 
+    backtracking which is the brute force as well as the most efficient
+    solution for this problem.
+        Backtracking is a general algorithm for finding (or some) solutions to
+    some computational problems. For this problem we can go through our input
+    array and we can explore the path of where we either add the element or 
+    don't. 
+        So for our solution we can first create our variables this being a 
+    result variable and a subsets variable. We can then declare our helper
+    function which would could be considered a dfs approach since we want
+    to explore as far down our possible subset combinations. The parameter
+    of our helper function can be the current index we are looking at, since
+    we want to see the possible combinations of whether we add it or don't.
+        So the first condition or base case we need to check for is if the
+    index is out of bounds, this can be done by i >= len(nums). If it isn't
+    then we can proceed with our backtracking algorithm. We can first append
+    to the result list a copy of our subsets list via the copy function. We
+    can then first get the path where we do add the element. This can be done
+    by appending the value held by the index to the subsets list, and then
+    recursively calling the helper function. Then we must explore the case
+    where we don't add the element. We can pop from the subsets list and then
+    recursively call the helper function with the index plus 1.
+        Then ouside of our function we can call the helper function with the
+    first index of our input array passed in, this being 0. We can then return
+    the result list.
+    
 P-lan:
+    1) Create our result and subsets list
+    2) Declare our dfs helper function
+    3) Check if our index is out of bounds
+    4) We can then append a shallow copy of our subsets list to the result list
+       using the copy() function
+    5) We can then explore the path where we do add the element
+        i) Add the value held at the index to the subsets list
+        ii) Recursively call the function with the index plus 1
+    6) We can then explore the path where we do not add the element
+        i) Pop the element we just added from the list
+        ii) Recursively call the function with the index plus 1
+    7) Outside the helper function we can call it with a 0 passed in
+    8) We can then return the result list
 
 
 I-mplement:
@@ -41,8 +83,30 @@ I-mplement:
 
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        pass
+        # Create our result and subsets list
+        result, subsets = [], []
 
+        # Declare our dfs helper function that takes in an index as a parameter
+        def dfs(i):
+            # Check if the index is out of bounds
+            if i >= len(nums):
+                # Append to the result list a copy of the subsets
+                return result.append(subsets.copy())
+            
+            # Otherwise, we can explore the decision where we add the element
+            subsets.append(nums[i])
+            dfs(i + 1)
+
+            # We can then check the decision where we do not add the element
+            subsets.pop()
+            dfs(i + 1)
+        
+        # We can call the helper function
+        dfs(0)
+
+        # Return the result list
+        return result
+    
 
     def display(self, nums: List[int]) -> None:
         print(f"Input: nums = {nums}")
